abb112f76b76b0f84d437eec0f1783d1ff6d9337
Vasilaki Totsili <vasilaki@totsili.com>
2026-07-15 19:14:02 +0200

    docs: add documentation comments to `Types.hpp`
    - Added documentation comments to `Types.hpp` to provide clear
      explanations for the enumerations defined in this file:
    - The `UserDefinedElectricalState` and `RawElectricalState` enums now
      include brief descriptions of their purpose, helping developers
      understand the difference between user-defined logic levels and raw
      pin states

--- Common/Types.hpp
+++ Common/Types.hpp
@@ -1,14 +1,20 @@
-
+///
+///@file Types.hpp
+///@brief Custom sparq types
+///
+///
 #pragma once
 
 namespace sparqCommon
 {
+    /// @brief  Electrical state when the user wants to specify what will be low or high, e.g. press of button is low but user wants it to be represented as high
     enum class UserDefinedElectricalState
     {
         ActiveLow = 0,
         ActiveHigh = 1,
     };
 
+    /// @brief Raw electrical state from the pin, e.g. button is pressed and is low and the raw will be low, same for high when a sensor picks up something and activates, undefined is when the pin is not connected and has high impedance
     enum class RawElectricalState
     {
         LOW = 0,
