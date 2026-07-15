0bc8c2a1b8776a4230b58037611eac1e84cbd5cb
drashhh <152928798+drashhh@users.noreply.github.com>
2026-07-15 19:32:35 +0200

    docs: add documentation comment to `ErrorCodes.hpp`
    - Added documentation comments to `ErrorCodes.hpp` to provide clear
      explanations for the enumerations defined in this file
    - The `OK` and `UNDEFINED_ELECTRICAL_LEVEL` error codes now have a
      brief explanation which could aid developers when diagnosing an
      error.

--- Common/ErrorCodes.hpp
+++ Common/ErrorCodes.hpp
@@ -1,10 +1,25 @@
+///
+///@file ErrorCodes.hpp
+///@brief Custom sparq ErrorCodes for handling different errors
+///
+///
+
 #pragma once
 
 namespace sparqCommon
 {
+    ///
+    ///@brief Custom error code values
+    ///
     enum class ErrorCodes
     {
+        ///
+        ///@brief No error
+        ///
         OK = 0,
+        ///
+        ///@brief Undefined Electrical level, usually used when you get an illegal (unusable) arguments when setting an electrical state
+        ///
         UNDEFINED_ELECTRICAL_LEVEL = 1,
 
     };
